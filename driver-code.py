1065871ed5515a2eaa80dcad58fe9cb4fa3b974d
aryamanchoudhary123 <aryaman.choudhary01@gmail.com>
2022-12-17 13:57:55 +0530

    version 2

--- driver-code.py
+++ driver-code.py
@@ -10,40 +10,38 @@ from tensorflow.keras.models import load_model
 
 
 def image_capture(img_name):
-    cam = cv2.VideoCapture(1)
+    cam = cv2.VideoCapture(0)
     ret,frame = cam.read()
     cv2.imwrite(img_name,frame)
     cam.release()
 
-def prediction(img_name):
+def prediction(img_name, model, class_names):
     img = cv2.imread(img_name) # change to direct camera feed
-    resize = tf.image.resize(img,(256,256))
-    pred_val = new_model.predict(np.expand_dims(resize/255,0))
-    return pred_val
+    image_resized = cv2.resize(image , (180,180))
+    image_resized= np.expand_dims(image_resized,axis=0)
 
-
-def action(pred_val):
+    pred_val = resnet_model.predict(image)
     print(pred_val)
+    output = class_names[np.argmax(x)]
+    return output
+
+
+def action():
 
     servo1.start(0)
-    if pred_val > 0.5 :
-        print("rotate motor as ideal")
-        # to open the funnel
-        angle = float(0)
-        servo1.ChangeDutyCycle((2+(angle/18)))
-        time.sleep(0.35)
-        servo1.ChangeDutyCycle(0)
-        # to close the funnel
-        angle = float(110)
-        servo1.ChangeDutyCycle((2+(angle/18)))
-        time.sleep(0.35)
-        servo1.ChangeDutyCycle(0)
-
-        servo1.stop()
-        GPIO.cleanup()
-    else:
-        print("Dont Rotate motor as non ideal")
-
+    print("rotate motor as ideal")
+    # to open the funnel
+    angle = float(0)
+    servo1.ChangeDutyCycle((2+(angle/18)))
+    time.sleep(0.35)
+    servo1.ChangeDutyCycle(0)
+    # to close the funnel
+    angle = float(110)
+    servo1.ChangeDutyCycle((2+(angle/18)))
+    time.sleep(0.35)
+    servo1.ChangeDutyCycle(0)
+    servo1.stop()
+    GPIO.cleanup()
 
 if __name__ == "__main__":
 
@@ -51,7 +49,10 @@ if __name__ == "__main__":
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(11,GPIO.OUT)
     servo1 = GPIO.PWM(11,50)
-    new_model = load_model("imageclassifiernewversionlive.h5")
+    model = load_model("resnet_model.h5")
+    class_names = ['Cricket Field', 'Forest Land', 'Grass', 'Hard Sand', 'Road', 'Tiles']
+
+    ideal_class = ['Cricket Field', 'Forest Land', 'Grass']
 
     print("takeoff in process")
     time.sleep(5) # delay for takeoff
@@ -59,7 +60,13 @@ if __name__ == "__main__":
     for i in range(100):
         image_name="test_flight_1_{}.png".format(i)
         image_capture(image_name)
-        pred_val = prediction(image_name)
-        action(pred_val)
+        pred_val = prediction(image_name,model,class_names)
+        if pred_val in ideal_class :
+            print("dropping seed as Ideal Land")
+            action()
+        else:
+            print("Non Ideal land detected")
         time.sleep(1)   # delay to move ahead
-        print("next part of land")
+        print("Onto next piece of land")
+
+    print("Flight Complete")
---
+++ image-capturing-code.py
@@ -0,0 +1,15 @@
+import cv2
+import time
+
+if __name__ == "__main__":
+
+    land_type = "cricket_field"     # ["cricket_field","forest_land","grass_land","hard_sand","road","tiles"]
+    cam = cv2.VideoCapture(0)
+
+    time.sleep(10) # 10 seconds delay for takeoff
+    for i in range(120): #120 Photos each
+        img_name = "{}_{}.png".format(land_type,i)
+        cv2.imwrite(img_name, frame)
+        time.sleep(1) # 1 second delay for each photo
+
+    cam.release()
